8d8d82b620c6ebe9a09abc50ca5e45b57d21c5c4
Sam Dalton <spd4nf@virginia.edu>
2018-09-25 13:55:13 -0500

    added bonus credit answer to functions as arguments

--- higher-order-functions.js
+++ higher-order-functions.js
@@ -24,7 +24,11 @@ function filter(arr, fn) {
         }
     }
     return newArray;
-}
+} 
+
+/* function filter(arr,fn){
+    return 
+} */
 
 // DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
 // First we setup an array of strings we plan to filter:
@@ -32,11 +36,11 @@ const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
 
 // We use your `filter` function here, capturing a new array into `filteredNames`
 // comprised of names that only begin with 'R'
-const filteredNames = filter(myNames, function(name) {
+//const filteredNames = filter(myNames, function(name) {
     // This is known as a "predicate function" - it's a function that 
     // only returns a boolean
-    return name[0] === 'R';
-});
+   // return name[0] === 'R';
+//});
 
-console.log(filteredNames) // => ['Rich', 'Ray']
+console.log(filter(myNames,names => names[0]=== 'R')); // => ['Rich', 'Ray']
 // <---- DO NOT EDIT BETWEEN THESE LINES
